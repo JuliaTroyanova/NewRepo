7093efac62f06b01bbcdd7497e66d2afaf939621
JuliaTroyanova <troyanovajulia88@gmail.com>
2020-04-02 20:45:04 +0300

    Initial commit

---
+++ example/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(253, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(127, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(467, 118);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 158);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(375, 218);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ example/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            byte[,] image_matr; // матрица полутонового изображения
+            int[] hist; //гистограмма
+            double[] ver; //вероятностная гистограмма
+            double[,] obr_image; // матрица обработанного изображения
+            double[] hist_new; //гистограмма
+            int gmax, gmin; //желаемое максимальное и минимальное значение яркости
+            int fmax, fmin; //максимальное и минимальное значение яркости изображения
+            int w_b, h_b; //ширина и высота изображения
+
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|Png Image|*.png";
+            ofd.Title = "Выберите файл изображения";
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(@ofd.FileName);
+            }
+
+            Bitmap ish_bitmap = (Bitmap)pictureBox1.Image;
+
+            w_b = ish_bitmap.Width;  //Ширина изображения
+            h_b = ish_bitmap.Height; //Высота изображения
+
+            image_matr = new byte[w_b, h_b];  //матрица изображения 
+            obr_image = new double[w_b, h_b];  //матрица изображения 
+
+            hist = new int[256];
+            hist_new = new double[256];
+
+            fmin = 1000;
+            fmax = -100;
+
+            for (int x = 0; x < w_b; x++)
+            {
+                for (int y = 0; y < h_b; y++)
+                {
+                    System.Drawing.Color c = ish_bitmap.GetPixel(x, y);//получаем цвет указанной точки
+                    int r = Convert.ToInt32(c.R);
+                    int b = Convert.ToInt32(c.B);
+                    int g = Convert.ToInt32(c.G);
+                    int brit = Convert.ToInt32(0.299 * r + 0.587 * g + 0.114 * b); //Перевод из RGB в полутон
+
+                    image_matr[x, y] = Convert.ToByte(brit);
+                }
+            }
+
+            for (int x = 0; x < w_b; x++)
+            {
+                for (int y = 0; y < h_b; y++)
+                {
+                    hist[image_matr[x, y]]++;
+
+                    if (image_matr[x, y] > fmax)
+                        fmax = image_matr[x, y];
+                    if (image_matr[x, y] < fmin)
+                        fmin = image_matr[x, y];
+                }
+            }
+
+
+            label1.Text = Convert.ToString(fmax); //Максимальная яркость
+            label2.Text = Convert.ToString(fmin); //Минимальная яркость
+
+
+            //Отображение гистограммы на компоненте chart
+            chart1.Series[0].Points.Clear();
+            for (int i = 0; i < 256; i++)
+            {
+                chart1.Series[0].Points.AddXY(i, hist[i]);
+            }
+
+            for (int x = 0; x < w_b; x++)
+            {
+                for (int y = 0; y < h_b; y++)
+                {
+                    if (obr_image[x, y] > 255)
+                        obr_image[x, y] = 255;
+                    if (obr_image[x, y] < 0)
+                        obr_image[x, y] = 0;
+                }
+            }
+
+            for (int x = 0; x < w_b; x++)
+            {
+                for (int y = 0; y < h_b; y++)
+                {
+                    obr_image[x, y] = (double)(255 - image_matr[x, y]);
+                }
+            }
+
+            Bitmap obr = (Bitmap)pictureBox1.Image;
+            for (int x = 0; x < w_b; x++)
+            {
+                for (int y = 0; y < h_b; y++)
+                {
+                    byte briteness = Convert.ToByte(obr_image[x, y]);
+                    System.Drawing.Color c = System.Drawing.Color.FromArgb(briteness, briteness, briteness);
+                    obr.SetPixel(x, y, c);
+                }
+            }
+
+        }
+        //private void OpenButton_Click(object sender, EventArgs e)
+        //{
+        //    OpenFileDialog ofd = new OpenFileDialog();
+        //    ofd.Filter = "Image Files (*.BMP; *.JPG; *.GIF; *.PNG) | *.BMP; *.JPG; *.GIF; *.PNG | All Files(*.*)|*.*";
+
+        //    if (ofd.ShowDialog() == DialogResult.OK)
+        //    {
+        //        try
+        //        {
+        //            pictureBox1.Image = new Bitmap(ofd.FileName);
+        //        }
+        //        catch
+        //        {
+        //            MessageBox.Show("Невозможно открыть выбранный файл", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //        }
+
+
+            }
+        }
